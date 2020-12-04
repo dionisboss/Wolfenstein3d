547a99a7f1f4b20fc34d42b490225d7d5b4c70e3
Gueren Drive <gdrive@ga-p5.msk.21-school.ru>
2020-12-04 19:04:38 +0300

    I displayed a square :D

--- src/main.c
+++ src/main.c
@@ -6,17 +6,63 @@
 /*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 16:08:09 by gdrive            #+#    #+#             */
-/*   Updated: 2020/12/04 16:08:12 by gdrive           ###   ########.fr       */
+/*   Updated: 2020/12/04 18:43:16 by gdrive           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 #include "mlx.h"
+
+typedef	struct			s_data
+{
+	void				*img;
+	void				*addr;
+	int					bits_per_pixel;
+	int					line_lenght;
+	int					endian;
+}						t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int		main(void)
 {
-	void *mlx_ptr = mlx_init();
-	void *win_ptr = mlx_new_window(mlx_ptr, 100, 100, "GDRIVE");
-	mlx_destroy_window(mlx_ptr, win_ptr);
+	void	*mlx;
+	void	*win;
+
+	t_data	img;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 600, 600, "Gdrive!");
+
+	img.img = mlx_new_image(mlx, 600, 600);
+
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
+
+	size_t	i = 0;
+	size_t	j = 0;
+	while (i < 300)
+	{
+		j = 0;
+		while (j < 300)
+		{
+			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+			j++;
+		}
+		i++;
+	}
+
+	mlx_put_image_to_window(mlx, win, img.img, 149, 149);
+
+	mlx_loop(mlx);
+
+
+
 	return (0);
 }
