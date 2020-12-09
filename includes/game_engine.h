059d327db6f458c69f2695463076fd1a32b55d3c
Gueren Drive <gdrive@ga-c1.msk.21-school.ru>
2020-12-09 20:05:18 +0300

    Moving (w),(a) and turns is realised. Needs fix bug in new_map.a, and realise N, S, E for player.

--- includes/game_engine.h
+++ includes/game_engine.h
@@ -6,84 +6,13 @@
 /*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 11:50:35 by gdrive            #+#    #+#             */
-/*   Updated: 2020/12/08 20:04:44 by gdrive           ###   ########.fr       */
+/*   Updated: 2020/12/09 19:38:42 by gdrive           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_ENGINE_H
 # define GAME_ENGINE_H
 # include "new_map.h"
-
-/*
-** For s_game_data.
-** MLX-data.
-*/
-
-typedef struct			s_mlx_data
-{
-	void				*mlx;
-	void				*win;
-}						t_mlx_data;
-
-/*
-** For s_game_data.
-** IMG-data.
-*/
-
-typedef struct			s_img_data
-{
-	void				*img;
-	void				*addr;
-	int					bits_per_pixel;
-	int					line_lenght;
-	int					endian;
-}						t_img_data;
-
-/*
-** For s_game_data.
-** Information about player.
-*/
-
-typedef struct			s_player
-{
-	double				pos_x;
-	double				pos_y;
-	double				dir_x;
-	double				dir_y;
-	char				start_view;
-}						t_player;
-
-/*
-** Keycodes.
-*/
-
-# define ESC 53
-# define W 13
-# define S 1
-# define A 0
-# define D 2
-
-typedef struct			s_key_f
-{
-	char				w;
-	char				s;
-	char				a;
-	char				d;
-}						t_keys_f;
-
-
-/*
-** Game-data.
-*/
-
-typedef struct			s_game_data
-{
-	t_mlx_data			mlx_data;
-	t_img_data			img_data;
-	t_player			player;
-	t_keys_f			keys;
-	t_map				map;
-}						t_game_data;
 
 /*
 ** For s_ray_data.
@@ -122,6 +51,7 @@ typedef struct			s_step
 }						t_step;
 
 /*
+** For s_game_data.
 ** Ray-data.
 */
 
@@ -142,11 +72,147 @@ typedef struct			s_ray_dir
 }						t_ray_dir;
 
 /*
-** Public functions.
+** For s_game_data.
+** MLX-data.
 */
 
-int		init_game_space(t_game_data *game_data);
-int		init_player(t_player *player, char **maze);
-int		raycasting_render(t_game_data *game_data);
+typedef struct			s_mlx_data
+{
+	void				*mlx;
+	void				*win;
+}						t_mlx_data;
+
+/*
+** For s_game_data.
+** IMG-data.
+*/
+
+typedef struct			s_img_data
+{
+	void				*img;
+	void				*addr;
+	int					bits_per_pixel;
+	int					line_lenght;
+	int					endian;
+}						t_img_data;
+
+/*
+** Keycodes.
+*/
+
+# define ESC 53
+# define W 13
+# define S 1
+# define A 0
+# define D 2
+
+/*
+** For s_game_data.
+** Key-flags.
+** 1 - key is pressed.
+** 0 - key is unpressed.
+*/
+
+typedef struct			s_key_f
+{
+	char				w;
+	char				s;
+	char				a;
+	char				d;
+}						t_keys_f;
+
+
+/*
+** For s_player.
+** Player position.
+*/
+
+# define MOVE_SPEED 0.1
+# define ROT_SPEED 0.05
+
+typedef struct			s_player_pos
+{
+	double				x;
+	double				y;
+}						t_player_pos;
+
+/*
+** For s_game_data.
+** Information about player.
+*/
+
+typedef struct			s_player
+{
+	t_player_pos		pos;
+	t_direction			dir;
+	char				start_view;
+}						t_player;
+
+/*
+** Game-data.
+*/
+
+typedef struct			s_game_data
+{
+	t_mlx_data			mlx_data;
+	t_img_data			img_data;
+	t_player			player;
+	t_keys_f			keys;
+	t_map				map;
+	t_ray_dir			ray;
+}						t_game_data;
+
+/*
+** ============================================================================
+**                             Public functions.
+** ============================================================================
+*/
+
+/*
+** Init game workspace.
+*/
+
+int						init_game_space(t_game_data *game_data);
+int						init_player(t_player *player, t_plane *plane, char **maze);
+
+/*
+** Game control.
+*/
+
+int						start_game(t_game_data *game_data);
+void					finish_game(t_game_data *game_data);
+
+/*
+** Render
+*/
+
+int						raycasting_render(t_game_data *game_data);
+
+/*
+** Drawing img.
+*/
+
+void					my_mlx_pixel_put(t_img_data *data, int x, int y, int color);
+void					draw_sky(t_game_data *game_data);
+void					draw_land(t_game_data *game_data);
+
+/*
+** Keyboard hanlder.
+*/
+
+int						handle_pressed_key(int keycode, t_game_data *game_data);
+int						handle_unpressed_key(int keycode, t_game_data *game_data);
+
+/*
+** Player moving.
+*/
+
+void					player_move(t_game_data *game_data);
+
+/*
+** ============================================================================
+** ============================================================================
+** ============================================================================
+*/
 
 #endif
